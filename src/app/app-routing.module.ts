1c102a88412cc7d45bd04241786a076a9d23e023
Olga Sochava <osochava@gmail.com>
2018-09-19 20:30:55 +0300

    fix to reload product list after selected category changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,29 +5,20 @@ import {NavigationTreeResolver} from './navigation-tree-resolver';
 import {MainWrapperComponent} from './main-wrapper/main-wrapper.component';
 
 const routes: Routes = [
-  {
-    path: '',
-    component: MainWrapperComponent,
-    resolve: {
-      categories: NavigationTreeResolver
-    }
-  },
-  {path: 'category', component: MainWrapperComponent,
-    resolve: {
-      categories: NavigationTreeResolver
-    }},
+  { path: '',  redirectTo: '/category/1907548051', pathMatch: 'full', runGuardsAndResolvers: 'paramsOrQueryParamsChange' },
   {
     path: 'category/:key',
     component: MainWrapperComponent,
     resolve: {
       categories: NavigationTreeResolver,
       products: AppResolver
-    }
+    },
+    runGuardsAndResolvers: 'paramsOrQueryParamsChange'
   }
 ]
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {onSameUrlNavigation: 'reload'})],
   exports: [ RouterModule ]
 })
 export class AppRoutingModule {}
--- src/app/list-of-products/list-of-products.component.ts
+++ src/app/list-of-products/list-of-products.component.ts
@@ -1,16 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {ApptusService} from '../apptus.service';
 import {Product} from '../product';
-import {ActivatedRoute} from '@angular/router';
+import {ActivatedRoute, NavigationEnd, Router} from '@angular/router';
 
 @Component({
   selector: 'app-list-of-products',
   templateUrl: './list-of-products.component.html',
   styleUrls: ['./list-of-products.component.css']
 })
-export class ListOfProductsComponent implements OnInit {
+export class ListOfProductsComponent implements OnInit, OnDestroy {
   products: Product[];
-  constructor(private route: ActivatedRoute) {}
+  navigationSubscription: any;
+
+  constructor(private route: ActivatedRoute, private router: Router) {
+    this.navigationSubscription = this.router.events.subscribe((e: any) => {
+      // If it is a NavigationEnd event re-initalise the component
+      if (e instanceof NavigationEnd) {
+        this.initialiseInvites();
+      }
+    });
+  }
+
+  initialiseInvites() {
+    this.products = this.route.snapshot.data['products'];
+  }
+
+  ngOnDestroy() {
+    // avoid memory leaks here by cleaning up after ourselves. If we
+    // don't then we will continue to run our initialiseInvites()
+    // method on every navigationEnd event.
+    if (this.navigationSubscription) {
+      this.navigationSubscription.unsubscribe();
+    }
+  }
 
   ngOnInit() {
     this.products = this.route.snapshot.data['products'];
